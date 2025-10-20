0117dec6252f5836242b39832351523f6db4dab2
Ledena21 <voronovae1112@gmail.com>
2025-10-20 17:01:13 +0500

    Исправили вложенность

--- src/main/java/org/example/ExitCommand.java
+++
@@ -1,7 +0,0 @@
-package org.example;
-
-public class ExitCommand {
-    public void execute() {
-        System.out.println("Спасибо за использование бота! До свидания!");
-    }
-}
--- src/main/java/org/example/HelpCommand.java
+++
@@ -1,7 +0,0 @@
-package org.example;
-
-public class HelpCommand {
-    public void execute() {
-        System.out.println("Я - бот, который выводит определения заимствованных слов.");
-    }
-}
