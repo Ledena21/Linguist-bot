73d07061d8d26f0497c10a0c6d066228074483c2
Ledena21 <voronovae1112@gmail.com>
2025-11-19 19:08:02 +0500

    Обновлен pom.xml для возможности деплоя

---
+++ src/main/java/pack/Main.java
@@ -0,0 +1,20 @@
+package pack;
+
+import org.telegram.telegrambots.meta.TelegramBotsApi;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
+
+public class Main {
+    public static void main(String[] args) {
+        try {
+            TelegramBotsApi botsApi = new TelegramBotsApi(DefaultBotSession.class);
+            TelegramBot bot = new TelegramBot();
+            botsApi.registerBot(bot);
+
+            System.out.println("Бот запустился");
+        } catch (TelegramApiException e) {
+            System.err.println("Бот не запустился: " + e.getMessage());
+            e.printStackTrace();
+        }
+    }
+}
